7c8fc1abbf785a3f6aa6e9ecc83425d7039d6b69
sssvulture <vulturesss13@gmail.com>
2023-01-07 23:03:25 +0300

    Make function to reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,7 @@
-const reverseString = function() {
-
+const reverseString = function(word) {
+  word = word.split('').reverse().join('');
+  console.log(word);
+  return word;
 };
 
 // Do not edit below this line
