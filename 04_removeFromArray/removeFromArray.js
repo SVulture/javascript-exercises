439056b081a414f82989b5bc83b9e4db93659b24
sssvulture <vulturesss13@gmail.com>
2023-01-07 23:56:21 +0300

    Make function removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...rest) { 
+ let words = [...rest];
+ for (let word of words) {
+    array = array.filter(x => x !== word)
+ }
+ return array
 };
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
