4e89f7f3759a66830a7d334e81b1a73f81110b01
sssvulture <vulturesss13@gmail.com>
2023-01-07 22:47:26 +0300

    Create function to repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(word, num) {
+  if (num >= 0) {
+  let result = word.repeat(num);   
+  return result;
+  } else return 'ERROR';
 };
+
 
 // Do not edit below this line
 module.exports = repeatString;
