86ef4010d7994d4aa9a76a93080d9582fb614829
sssvulture <vulturesss13@gmail.com>
2023-01-08 21:12:41 +0300

    Checl palindromes

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  let check = phrase.split('');
+  check = check.reverse().filter(x => /[A-Za-z]/.test(x)).join('');
+  phrase = phrase.split('').filter(x => /[A-Za-z]/.test(x)).join('');  
+  let res;  
+  phrase.toLowerCase() === check.toLowerCase() ? res = true : res = false
+  return res
 };
 
 // Do not edit below this line
