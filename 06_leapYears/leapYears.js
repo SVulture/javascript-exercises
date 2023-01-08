e0cdc5c312822d6478e13ccced8656c97cfe7664
sssvulture <vulturesss13@gmail.com>
2023-01-08 19:45:03 +0300

    Create function to check leap years

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,16 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+let res;
+year % 4 !== 0 
+  ? res = false
+  : year % 400 === 0
+  ? res = true 
+  : year % 100 === 0
+  ? res = false
+  : res = true
+return res
 };
+
+leapYears(2000);
 
 // Do not edit below this line
 module.exports = leapYears;
