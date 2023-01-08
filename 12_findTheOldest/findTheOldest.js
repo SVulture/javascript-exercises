ecba92a579fad49b785dd350d92b1dff2f0ca1a8
sssvulture <vulturesss13@gmail.com>
2023-01-09 00:44:18 +0300

    Make function to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,38 @@
-const findTheOldest = function() {
+/*const people = [
+    {
+      name: "Carly",
+      yearOfBirth: 1942,
+      yearOfDeath: 1970,
+    },
+    {
+      name: "Ray",
+      yearOfBirth: 1962,
+      yearOfDeath: 2011,
+    },
+    {
+      name: "Jane",
+      yearOfBirth: 1912,
+      yearOfDeath: 1941,
+    },
+  ]*/
 
+const findTheOldest = function(people) {
+  
+  for (let person of people) {
+    if (person.yearOfDeath) {
+        person.age = person.yearOfDeath - person.yearOfBirth;
+    } else {
+        const today = new Date();
+        const year = today.getFullYear();
+        person.age = year - person.yearOfBirth;
+    }    
+  }  
+  people = people.sort((a,b) => b.age - a.age);
+  //console.log(people);
+  return people[0];
 };
+
+//findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
