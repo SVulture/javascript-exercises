50daf334721a4a4b07f67cf62cb25ad086e297e0
sssvulture <vulturesss13@gmail.com>
2023-01-08 21:29:11 +0300

    Make function to count fibonacci

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,13 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+  let fib =[0];
+  fib[1] = 1;
+  fib[2] = 1;
+  if (num > 0) {
+    for (let i = 3; i <= num; i++) {
+      fib[i] = fib[i-1] + fib [i-2];
+    }
+    return fib[num]
+  } else return 'OOPS'
 };
 
 // Do not edit below this line
