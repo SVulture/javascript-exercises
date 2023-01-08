b74f050a57c07020ccc5c3c8314c295b1a00afce
sssvulture <vulturesss13@gmail.com>
2023-01-09 00:45:24 +0300

    Fix the function to findTheOldest

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,21 +1,3 @@
-/*const people = [
-    {
-      name: "Carly",
-      yearOfBirth: 1942,
-      yearOfDeath: 1970,
-    },
-    {
-      name: "Ray",
-      yearOfBirth: 1962,
-      yearOfDeath: 2011,
-    },
-    {
-      name: "Jane",
-      yearOfBirth: 1912,
-      yearOfDeath: 1941,
-    },
-  ]*/
-
 const findTheOldest = function(people) {
   
   for (let person of people) {
@@ -27,12 +9,9 @@ const findTheOldest = function(people) {
         person.age = year - person.yearOfBirth;
     }    
   }  
-  people = people.sort((a,b) => b.age - a.age);
-  //console.log(people);
+  people = people.sort((a,b) => b.age - a.age); 
   return people[0];
 };
-
-//findTheOldest(people);
 
 // Do not edit below this line
 module.exports = findTheOldest;
