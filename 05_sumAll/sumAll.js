f9b677a5a8c59841cae92252d60bf46e4947a356
sssvulture <vulturesss13@gmail.com>
2023-01-08 16:32:21 +0300

    Make function to sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,13 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+let sum = 0;    
+if (num1 < 0 || num2 < 0 || num2 !== +num2 || num1 !== +num1) {  
+  return 'ERROR'
+  } else {
+    for (let i = Math.min(num1, num2); i <= Math.max(num1, num2); i++) {
+      sum +=i;      
+    }    
+  }  
+return sum
 };
-
-// Do not edit below this line
+ 
 module.exports = sumAll;
